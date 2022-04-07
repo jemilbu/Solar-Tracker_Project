6c06f09642094acde899e9c8d278a8bb41b7e57e
Jacob Milburn <74629523+jemilbu@users.noreply.github.com>
2022-04-07 18:56:13 -0500

    Pulled code together, need to combine efficiently

--- SolarTracker/SolarTracker.ino
+++ SolarTracker/SolarTracker.ino
@@ -11,66 +11,88 @@
 #include "ButtonDebounce.h"
 
 //  Set up Global Variables
-
-// Defines for LCD
-LiquidCrystal LcdDriver(A5, A4, 5, 6, 7, 8);
-
-//  Set up Program
-void setup()
-{
-    EncoderInitialize();
-    ButtonInitialize();
-}
-
-//  Main Loop
-void loop()
-{
-}
-
-
-/* Code from Lecture: 
-
-
-
-Servo myservo;
 int pos = 90;
 int sens1 = 0;
 int sens2 = 1;
 int error = 5;
 int servopin = 10;
-// LCD Display
-LiquidCrystal lcd(A5, A4, 5, 6, 7, 8);
-// Software Timer for control 2 Hz
+int EncoderTracking = 0;
+int Lcd_Update = 0;
+
+//  Software Timer for control 2 Hz
 unsigned long ControlTimer;
 #define CONTROL_INTERVAL 500
-// put your setup code here, to run once:
-void setup()
-{
-    myservo.attach(servopin);
-    pinMode(sens1, INPUT);
-    pinMode(sens2, INPUT);
-    pinMode(servopin, OUTPUT);
-    myservo.write(pos);
-    delay(2000);
-    // LCD display setup
-    lcd.begin(16, 2);
-    lcd.clear();
-    // Software timer setup.
-    ControlTimer = millis();
-} // end of setup.
+
 // Parameters of search algorithm
 #define LightOff 500
 #define LightTolerance 25
+
+// Timer to control Clock, TxMorseCode, Led and Encoder.
+#define CLOCK_TIME 1000
+#define DOT_TIME 50
+#define ENCODER_INTERVAL 250
+unsigned long ClkTimer, TxTimer, EncoderTimer;
+unsigned long INTERVAL = 1000;
+// setup code, ran once:
+
+//  Defines for LCD
+LiquidCrystal LcdDriver(A5, A4, 5, 6, 7, 8);
+
+//  Defines for Servo
+Servo myservo;
+
+//  Defines for LCD Display
+LiquidCrystal lcd(A5, A4, 5, 6, 7, 8);
+
+// Setup Program
+void setup()
+{
+    //  Call .h file Inits
+    EncoderInitialize();
+    ButtonInitialize();
+
+    //  Attach Servo
+    myservo.attach(servopin);
+
+    // Set Pin types
+    pinMode(sens1, INPUT);
+    pinMode(sens2, INPUT);
+    pinMode(servopin, OUTPUT);
+
+    //  Write servo to starting position and wait for motion to complete
+    myservo.write(pos);
+    delay(2000);
+
+    // LCD display setup
+    lcd.begin(16, 2);
+    lcd.clear();
+
+    // Software timer setup.
+    ControlTimer = millis();
+
+    EncoderTracking = encoderPosition;
+
+    // Set initial time
+    CLK_Hours = 9;
+    CLK_Minutes = 58;
+    CLK_Seconds = 58;
+
+    // Lcd and Serial
+    LcdDriver.begin(16, 2);
+    Serial.begin(9600); // Serial Port on
+} // end of setup.
+
+// Start Main loop
 void loop()
 {
-    // put your main code here, to run repeatedly:
     int Total, MidPoint;
-    if (millis() - ControlTimer >= CONTROL_INTERVAL)
+    if (millis() - ControlTimer >= CONTROL_INTERVAL) //  If specified interval has passed
     {
         // Read in light levels.
         Total = analogRead(sens1);
         MidPoint = analogRead(sens2);
         // Send results to display.
+        /*
         lcd.clear();
         lcd.print("Tot. ");
         lcd.print(Total);
@@ -81,6 +103,7 @@ void loop()
         lcd.print(MidPoint);
         lcd.print("->");
         lcd.print(5.0 * (float)MidPoint / 1024.0, 3);
+        */
         // Update timer.
         ControlTimer += CONTROL_INTERVAL;
         // check if motion is needed.
@@ -94,52 +117,6 @@ void loop()
                 myservo.write(pos);
             } // end of position update.
     }         // end of timer if
-} // end of loop.
-
-*/
-
-/* Clock from lecture:
-Appendix F: Main Arduino Code for Morse Code Transmit
-#include <LiquidCrystal.h>
-// Defines for LCD
-LiquidCrystal LcdDriver(A5, A4, 5, 6, 7, 8);
-#include "ASCIIToMorseCode.h"
-#include "TriColorLed.h"
-#include "ClockBasics.h"
-#include "ButtonDebounce.h"
-#include "EncoderMonitor.h"
-int EncoderTracking = 0;
-// Timer to control Clock, TxMorseCode, Led and Encoder.
-#define CLOCK_TIME 1000
-#define DOT_TIME 50
-#define LED_FLASH_TIME 1000
-#define ENCODER_INTERVAL 250
-unsigned long ClkTimer, TxTimer, LedTimer, EncoderTimer;
-unsigned long INTERVAL = 1000;
-// setup code, ran once:
-void setup()
-{
-    SetUpLED();
-    pinMode(MorseCodePin, OUTPUT); // Set to control beeper
-    // Set up clock
-    // first the inputs for setting the clock.
-    ButtonInitialize(4); // connect to push button
-    EncoderInitialize();   // Monitor encoder.
-    EncoderTracking = EncoderValue;
-    // Set initial time
-    CLK_Hours = 9;
-    CLK_Minutes = 58;
-    CLK_Seconds = 58;
-    // Lcd and Serial
-    LcdDriver.begin(16, 2);
-    Serial.begin(9600);    // Serial Port on
-    TxTimer = millis();    // Timer for Morse Code transmitting.
-    LedTimer = millis();   // Timer for led flash
-}
-int Lcd_Update = 0;
-// main code, ran repeatedly:
-void loop()
-{
     // Clock Control
     if (millis() - ClkTimer >= CLOCK_TIME)
     {
@@ -148,7 +125,7 @@ void loop()
         Lcd_Update = 1; // Set flag to update the display
         ClkTimer += CLOCK_TIME;
         LcdDriver.clear();
-        SendClock();  // Place time
+        SendClock(); // Place time
         // Set the cursor to indicate what is being set.
         switch (clockState)
         {
@@ -167,47 +144,22 @@ void loop()
         LcdDriver.blink();
         Lcd_Update = 0; // Clear lcd update flag.
     }
-    // Flash LED
-    if (millis() - LedTimer >= INTERVAL)
-    {
-        SwitchLED();
-        // Update timer
-        LedTimer += INTERVAL;
-        // Change interval after each cycle.
-        if (LedState == RedLedPin)
-        {
-            INTERVAL -= 50;
-            if (INTERVAL < 100)
-                INTERVAL = 500;
-        } // end of update of interval change
-    } // end of led timer.
-    // Timer to control time of beeper on and off.
-    if (millis() - TxTimer >= DOT_TIME)
-    {
-        // Based on state,
-        if (TxNextState())
-            digitalWrite(MorseCodePin, HIGH); // Beeper on.
-        else
-            digitalWrite(MorseCodePin, LOW); // beeper off.
-        TxTimer += DOT_TIME; // Update timer
-    }
-    // Watch button and encoder to set clock
-    if (ButtonNextState( digitalRead(4)) == 1) // Button pressed,
-        SettingClock(1, 0);  // So update state.
-    else if ((EncoderValue - EncoderTracking) >= 4)
-    { // The encoder has moved by one detent
-        SettingClock(0, -1);  // Indicate positive travel
-        LcdDriver.clear();
-        SendClock();  // Place time
-        EncoderTracking += 4;   // and move tracking variable.
-    }
-    else if ((EncoderValue - EncoderTracking) <= -4)
-    { // The encoder has moved by one detent in the opposite direction
-        SettingClock(0, +1);  // Indicate negative travel
-        LcdDriver.clear();
-        SendClock();  // Place time
-        EncoderTracking -= 4;   // and Update Tracking accordingly
-    }
-}// End of loop.
 
-*/
+    // Watch button and encoder to set clock
+    if (ButtonNextState(digitalRead(4)) == 1) // Button pressed,
+        SettingClock(1, 0);                   // So update state.
+    else if ((encoderPosition - EncoderTracking) >= 4)
+    {                        // The encoder has moved by one detent
+        SettingClock(0, -1); // Indicate positive travel
+        LcdDriver.clear();
+        SendClock();          // Place time
+        EncoderTracking += 4; // and move tracking variable.
+    }
+    else if ((encoderPosition - EncoderTracking) <= -4)
+    {                        // The encoder has moved by one detent in the opposite direction
+        SettingClock(0, +1); // Indicate negative travel
+        LcdDriver.clear();
+        SendClock();          // Place time
+        EncoderTracking -= 4; // and Update Tracking accordingly
+    }
+} // end of loop.
